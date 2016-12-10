d4c7fb1cd3d75ea82a9e9897314468aa5fa4c655
a7web <a.v.abramov55@gmail.com>
2016-12-11 03:56:56 +0600

    Create Strings.js

---
+++ nls/ru/Strings.js
@@ -0,0 +1,34 @@
+define( {
+	// EXTENSION.
+	EXTENSION_NAME: "SFTP Upload",
+    
+  // MENUS
+  UPLOAD_MENU_NAME: "Загрузить с помощью SFTP",
+  DOWNLOAD_MENU_NAME: "Скачать с сервера",
+	
+	// GENERAL.
+	YES:    "Да",
+	NO:     "Нет",
+	OK:     "Ок",
+	CANCEL: "Отмена",
+	UPLOAD: "Загрузить",
+	SKIP:   "Пропустить",
+	
+	// TOOLBAR.
+	SERVER_SETUP: "Настройки",
+	UPLOAD_ALL: "Загрузить Всё",
+	SKIP_ALL: "Пропустить Всё",
+	
+	// SETTINGS DIALOG.
+	SETTINGS_DIALOG_TITLE:        "Настройки SFTP",
+	SETTINGS_DIALOG_TYPE:    	  "Тип",
+	SETTINGS_DIALOG_TYPE_FTP: 	  "FTP",
+	SETTINGS_DIALOG_TYPE_SFTP:    "SFTP(SSH)",
+	SETTINGS_DIALOG_HOST: 		  "Хост",
+	SETTINGS_DIALOG_PORT: 		  "Порт",
+	SETTINGS_DIALOG_USERNAME: 	  "Имя пользователя",
+	SETTINGS_DIALOG_PASSWORD: 	  "Пароль",
+    SETTINGS_DIALOG_RSAMSG:       "Путь RSA ключа",
+	SETTINGS_DIALOG_PATH: 		  "Директория на сервере",
+    SETTINGS_UPLOAD_ON_SAVE:      "Загрузить и сохранить"
+} );
